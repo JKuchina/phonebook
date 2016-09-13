8acb6ce55b327a3402e57a96517d273a5957b7a1
Yulia <kuchina@gmail.com>
2016-09-13 11:14:55 +1000

    scaffolding

---
+++ grails-app/controllers/phonebook/PersonController.groovy
@@ -0,0 +1,7 @@
+package phonebook
+
+class PersonController {
+
+    static scaffold = Person
+
+}
---
+++ grails-app/domain/phonebook/Person.groovy
@@ -0,0 +1,11 @@
+package phonebook
+
+class Person {
+
+    String firstName
+    String lastName
+    String phone
+
+    static constraints = {
+    }
+}
